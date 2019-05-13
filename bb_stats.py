984dfd23af6ca03de0c6431b7eca4f798029698e
Bob Staten <smilleyy_1999@yahoo,com>
2019-05-13 11:14:53 -0600

    update game data

--- bb_stats.py
+++ bb_stats.py
@@ -25,10 +25,13 @@ def stats_j(hits,abs,rbis):
 # execution
 # TODO create a file to store data, and use it to update average.
 # here's the problem. I want to store stats in a file instead of keeping them in the code itself as below
-stats_j(0,3,1)
-stats_j(1,3,2)
-<<<<<<< HEAD
-print(stats_j(2,3,1))
-=======
-print (stats_j(2,3,1))
->>>>>>> f06ed424145d73e35741e37e2b02c35b7b9c5dd7
+stats_j(0,3,0) # 4/4/19
+stats_j(2,3,1) # 4/6/19
+stats_j(1,3,2) # 4/9/19
+stats_j(1,4,1) # 4/15/19
+stats_j(2,3,2) # 4/18/19
+stats_j(2,3,2) # 4/23/19
+stats_j(2,3,1) # 4/25/19
+stats_j(2,3,0) # 4/27/19
+stats_j(5,5,3) # 5/4/19
+print(stats_j(1,4,0)) # 5/11/19
